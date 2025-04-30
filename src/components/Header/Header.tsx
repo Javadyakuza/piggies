810500ca4d58416637de25add88597e4f0137a29
Kamyar Poureatesam <kmyr.rp@gmail.com>
2025-05-01 01:25:48 +0330

    feat: remove unused components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,34 @@
 "use client";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faCheck, faUser } from "@fortawesome/free-solid-svg-icons";
 import { useSignal, initData } from "@telegram-apps/sdk-react";
 import "./styles.css";
 import { TonConnectButton } from "@tonconnect/ui-react";
+import { Button } from "@telegram-apps/telegram-ui";
+import { faCopy } from "@fortawesome/free-solid-svg-icons";
+import { useTranslations } from "next-intl";
+import { useEffect, useState } from "react";
 
 export default function Header() {
   const initDataState = useSignal(initData.state);
   const userData = initDataState?.user;
+  const t = useTranslations("i18n");
+  const [isRefLinkCopied, setIsRefLinkCopied] = useState(false);
+
+  const handleCopyRefLink = () => {
+    if (isRefLinkCopied) return;
+    setIsRefLinkCopied(true);
+    navigator.clipboard.writeText("Sample ref link");
+  };
+
+  useEffect(() => {
+    if (isRefLinkCopied) {
+      setTimeout(() => {
+        setIsRefLinkCopied(false);
+      }, 800);
+    }
+  }, [isRefLinkCopied]);
 
   return (
     <div className="main-header">
@@ -21,7 +41,20 @@ export default function Header() {
             {userData?.firstName} {userData?.lastName}
           </h4>
         </div>
-        <TonConnectButton className="ton-connect-page__button-connected" />
+        <div className="ref-link">
+          <Button
+            onClick={handleCopyRefLink}
+            className="copy-ref-link-Button primary-btn"
+          >
+            <span>
+              {isRefLinkCopied ? t("refLinkCopied") : t("action.copyRefLink")}
+            </span>{" "}
+            <FontAwesomeIcon
+              icon={isRefLinkCopied ? faCheck : faCopy}
+              size="lg"
+            />
+          </Button>
+        </div>
       </div>
       <hr />
     </div>
