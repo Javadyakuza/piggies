ffe42395974fb786e3812ebfd8ea4913c6cf1bcd
Kamyar Poureatesam <kmyr.rp@gmail.com>
2025-05-18 09:30:15 +0330

    feat: design footer toolbar

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import "./styles.css";
+import { useTranslations } from "next-intl";
+import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
+
+export function Footer() {
+  const pathname = usePathname();
+  const router = useRouter();
+  const [activePage, setActivePage] = useState(pathname);
+
+  const t = useTranslations("i18n");
+  const footerItems = [
+    {
+      title: t("footerContent.bank"),
+      icon: "/imgs/icons/bank.png",
+      link: "/",
+      key: "/store",
+    },
+    {
+      title: t("footerContent.friends"),
+      icon: "/imgs/icons/friends.png",
+      link: "/",
+      key: "/profile",
+    },
+    {
+      title: t("footerContent.history"),
+      icon: "/imgs/icons/history.png",
+      link: "/",
+      key: "/history",
+    },
+  ];
+
+  const handlePageChange = (page: string) => {
+    setActivePage(page);
+    router.replace(page);
+  };
+  return (
+    <div className="main-footer-container">
+      {footerItems.map((item, i) => (
+        <div
+          onClick={() => {
+            handlePageChange(item.key);
+          }}
+          className={`footer-item ${activePage === item.key ? "--active" : ""}`}
+          key={i}
+        >
+          <Image src={item.icon} alt={item.title} width={24} height={24} />
+          <h4>{item.title}</h4>
+        </div>
+      ))}
+    </div>
+  );
+}
