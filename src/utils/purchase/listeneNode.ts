85b657a27ea34143c03de796c61104c248d7d666
Javadyakuza <javadyakuzaa@gamil.com>
2025-06-13 02:53:39 +0330

    Fix: WIP: Add withdraw event system to contracts

--- src/utils/purchase/listeneNode.ts
+++ src/utils/purchase/listeneNode.ts
@@ -31,8 +31,9 @@ import {
   upgradeUserPigAddress,
 } from "./dbOps";
 import { PigLevel } from "@/models/pigs";
+import { WithdrawFromNftPig } from "../../../wrappers/Pig";
 
-async function catchEvents(listenAddress: Address, afterLt: bigint) {
+async function catchPigShopEvents(listenAddress: Address, afterLt: bigint) {
   const tc = getTonClient();
   const tac = getTonApiClient();
   const adminWallet = await getAdminWallet(tc);
@@ -184,12 +185,174 @@ async function catchEvents(listenAddress: Address, afterLt: bigint) {
   return txs.transactions[txs.transactions.length - 1].lt;
 }
 
-export async function listenForever() {
+export async function listenPigShopForever() {
   let lastLt = BigInt(0);
   while (true) {
-    lastLt = await catchEvents(
+    lastLt = await catchPigShopEvents(
       Address.parse(process.env.NEXT_PUBLIC_PIG_SHOP_ADDRESS!),
       lastLt
     );
   }
-} // why with loop while we have setInterval ???!!!
+}
+
+
+async function catchPigEvents(listenAddress: Address, afterLt: bigint) {
+  const tc = getTonClient();
+  const tac = getTonApiClient();
+  const adminWallet = await getAdminWallet(tc);
+  const pig = tc.open(
+    Pig.fromAddress(
+      Address.parse(process.env.NEXT_PUBLIC_PIG_SHOP_ADDRESS!)
+    )
+  );
+
+  const txs = await tac.blockchain.getBlockchainAccountTransactions(
+    listenAddress,
+    {
+      limit: 10,
+    }
+  );
+
+  if (txs.transactions.length === 0) return afterLt;
+
+  let event: extendedPigUpgradeEvent | extendedPigApprovalEvent | PigCreationEvent | undefined;
+  for (const tx of txs.transactions) {
+    // tx must be successful
+    if (!tx.computePhase?.success || !tx.actionPhase?.success || tx.aborted)
+      continue;
+    // tx must have a body
+    if (tx.inMsg?.rawBody === undefined) continue;
+    for (const msg of tx.outMsgs) {
+      if (
+        msg.msgType == "ext_out_msg" &&
+        msg.decodedOpName == "UpgradePig" &&
+        msg.rawBody
+      ) {
+        try {
+          event = {
+            ...loadUpgradePig(msg.rawBody?.asSlice()),
+            tx_hash: tx.hash,
+          };
+        } catch (e) {
+          continue;
+        }
+      } else if (
+        msg.msgType == "ext_out_msg" &&
+        "pig_approval" &&
+        msg.rawBody
+      ) {
+        try {
+          event = {
+            ...loadPigApprovalEvent(msg.rawBody?.asSlice()),
+            tx_hash: tx.hash,
+          };
+        } catch (e) {
+          continue;
+        }
+      }
+    }
+  }
+
+  // handle if any purchase related events was found
+  if (event && event.userAddress) {
+    // finding the relative user and its reward receiver
+    let pig_data = await getPigs(event.userAddress.toRawString());
+    let bh: bountyHuntersResponse = await findUsersBountyHunters(
+      event.userAddress.toRawString(),
+      pig_data.new_pig_level
+    );
+    // identifying the event type
+    if (event.$$type == "UpgradePig") {
+      let tx_id = TxId.create(
+        event.userAddress.toRawString(),
+        pig_data.old_pig_level
+      );
+      if (
+        !(await isDuplicatePurchase(
+          event.userAddress.toRawString(),
+          pig_data.old_pig_level
+        ))
+      ) {
+        // the purchase have been initiated and the tokens are received by the "pigsShop" contract
+        console.log(
+          `Upgrade pig request initiated wallet address${event.userAddress}`
+        );
+
+        // sending the approval message to the pig shop to distribute the tokens to the bounty hunters
+        await sendPigApproval(
+          bh,
+          adminWallet,
+          pigShop,
+          pig_data.address,
+          event.userAddress.toRawString()
+        );
+
+        // update users transaction history
+        await initTxHistory({
+          tx_id,
+          tx_hash: event.tx_hash,
+          wallet_address: event.userAddress.toRawString(),
+          request_status: "PigUpgradePending",
+          upgraded_pig_level: pig_data.new_pig_level,
+        });
+      }
+    }
+    if (event.$$type == "PigApprovalEvent") {
+      console.log(
+        `approval received for wallet address ${event.userAddress} pig upgrade request`
+      );
+      event.referrer = Dictionary.empty<Address, bigint>().set(
+        event.referrerNftAddress,
+        event.referrerAmount
+      );
+      // the tokens have been distributed, updating the db
+      await updateBountyHuntersBalances({
+        referrer: event.referrer,
+        users: event.userBountyHunters,
+        admins: event.adminsShares,
+      });
+
+      // the user current pig should be upgraded
+      await upgradeUserPig(event.userAddress.toRawString());
+
+      // update users transaction history
+      await updateTxHistory({
+        tx_id: TxId.create(
+          event.userAddress.toRawString(),
+          pig_data.old_pig_level
+        ),
+        tx_hash: event.tx_hash,
+        wallet_address: event.userAddress.toRawString(),
+        request_status: "PigPurchaseApproved",
+        upgraded_pig_level: pig_data.new_pig_level,
+      });
+
+      event.referrer = Dictionary.empty<Address, bigint>().set(
+        event.referrerNftAddress,
+        event.referrerAmount
+      );
+      // update the referrals rewards history
+      let updateRes = await updateReferralsRewardsHistory(event);
+
+      if (!updateRes) {
+        throw new Error("Failed to update referrals rewards history!");
+      }
+    }
+    if (event.$$type == "PigCreationEvent") {
+      // updating the user pig address on the db
+      await upgradeUserPigAddress(event.userAddress.toRawString(), event.nft.toRawString());
+    }
+    
+  }
+
+  return txs.transactions[txs.transactions.length - 1].lt;
+}
+export async function listenPigForever() {
+  let lastLt = BigInt(0);
+  while (true) {
+    lastLt = await catchPigEvents(
+      Address.parse(process.env.NEXT_PUBLIC_PIG_ADDRESS!),
+      lastLt
+    );
+  }
+}
