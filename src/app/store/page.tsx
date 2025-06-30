7bc8e18dacd6f6b4b3657c4c073c8a13090a2436
Ivan Kotelnikov <ivan.d.kotelnikov@gmail.com>
2025-07-01 00:21:25 +0200

    rm back button

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -511,7 +511,7 @@ export default function StorePage() {
       {isConfirmModalOpen ? (
         confirmModal
       ) : (
-        <Page>
+        <Page back=false>
           <div className="bank-container">{mainPage}</div>
         </Page>
       )}
