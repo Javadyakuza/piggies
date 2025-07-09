82cff88eefdeef0a085783fad0a17bd6a7b28a17
Ivan Kotelnikov <ivan.d.kotelnikov@gmail.com>
2025-07-09 08:24:14 +0200

    version

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -66,7 +66,7 @@ export default function Header() {
           <h4 className="full-name">
             {userData?.firstName} {userData?.lastName}
           </h4>
-          <span style={{ fontSize: "1rem" }}> v3.0.1-tst </span>
+          <span style={{ fontSize: "1rem" }}> v3.0.2-tst </span>
         </div>
         <div className="actions-container">
           <div>
