71e68a41d8fb6427293d41a57551e9dc12efa309
aaronneocognitron <aaron.neocognitron@gmail.com>
2025-07-19 16:50:10 +0300

    increment version

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -66,7 +66,7 @@ export default function Header() {
           <h4 className="full-name">
             {userData?.firstName} {userData?.lastName}
           </h4>
-          <span style={{ fontSize: "1rem" }}> v3.0.3-tst </span>
+          <span style={{ fontSize: "1rem" }}> v3.1.0-tst </span>
         </div>
         <div className="actions-container">
           <button
